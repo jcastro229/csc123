84abca19dc057d413fa7e309ae0da679abedb9ee
jcastro229 <jcastro229@10.163.16.176>
2023-09-02 15:40:12 -0700

    Push to remote

--- CSC123_1/src/HelloWorld.java
+++ CSC123_1/src/HelloWorld.java
@@ -1,7 +1,8 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.print("Hello World! How Are You?");
+		System.out.println("Hello World! How Are You?");
+		System.out.println("Second Line");
 	}
 	
 
